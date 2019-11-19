8ecccce19d7da747a2f29f67ae95bc6ad37051df
Therydusone <przem.pajak@gmail.com>
2019-11-19 14:21:29 +0100

    making form to sign in and sign out

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import {Switch} from "react-router-dom";
 import {Route} from "react-router-dom";
+import Header from  "./components/header/header.component.jsx"
+import SignInAndSignOutPage from './pages/sign-in-and-sign-out/sign-in-and-sign-out.component'
 
 
 function App() {
   return (
     <div>
+    <Header />
     <Switch>
     <Route exact path='/' component={HomePage} />
     <Route path='/shop' component={ShopPage} />
+    <Route path='/signin' component={SignInAndSignOutPage} />
     </Switch>
     </div>
   );
