afe15e9b01d1e0157d26c6acf9d51f74a0bcf077
Therydusone <przem.pajak@gmail.com>
2019-11-27 18:35:59 +0100

    finished sign-in and sign-up

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import './sign-in.styles.scss';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle} from '../../firebase/firebase.utils'
+import { auth, signInWithGoogle} from '../../firebase/firebase.utils'
 class SignIn extends React.Component {
     constructor(props) {
         super(props)
@@ -12,10 +12,16 @@ class SignIn extends React.Component {
             password: ''
         }
     }
-handleSubmit = event => {
+handleSubmit = async event => {
     event.preventDefault();
+    const { email, password } = this.state;
 
+    try {
+        await auth.signInWithEmailAndPassword(email, password);
     this.setState({email:'', password:""})
+} catch (error) {
+    console.log(error);
+}
 }
 
 handleChange = event => {
