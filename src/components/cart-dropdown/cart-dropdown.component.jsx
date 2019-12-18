a77c46caadf8e197bcbc4681ab3ab4cc1c56e004
Therydusone <przem.pajak@gmail.com>
2019-12-18 22:46:46 +0100

    cart item component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 
-import CustomButtom from '../custom-button/custom-button.component';
+import { connect } from 'react-redux'
 
+import CustomButtom from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component'
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems}) => (
     <div className='cart-dropdown'>
-    <div clasName='cart-items'/>
+    <div clasName='cart-items'>
+    {
+        cartItems.map(cartItem => (<CartItem key={cartItem.id} item={cartItem}/>))
+    }
+    </div>
     <CustomButtom>GO TO CHECKOUT</CustomButtom>
     </div>
 )
-
-export default CartDropdown;
+ const mapStateToProps = ({cart: { cartItems }})=> ({
+     cartItems
+ })
+export default connect(mapStateToProps)(CartDropdown);
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -7,6 +7,7 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
+
 const CartIcon = ({ toggleCartHidden }) => (
   <div className='cart-icon' onClick={toggleCartHidden}>
     <ShoppingIcon className='shopping-icon' />
