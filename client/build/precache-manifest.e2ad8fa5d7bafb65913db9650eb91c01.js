70f452e3af49037a797ce1487b893b71bf101dc5
Therydusone <przem.pajak@gmail.com>
2019-12-21 20:49:11 +0100

    express server to get payments

---
+++ client/build/precache-manifest.e2ad8fa5d7bafb65913db9650eb91c01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31c648921a7c47f1caff5bf27e4a8e62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c12b5479be4f5a07c2ef",
+    "url": "/static/css/main.19fd4968.chunk.css"
+  },
+  {
+    "revision": "24e7bbfed01c3b574f15",
+    "url": "/static/js/2.16785aae.chunk.js"
+  },
+  {
+    "revision": "c12b5479be4f5a07c2ef",
+    "url": "/static/js/main.46f2c8cf.chunk.js"
+  },
+  {
+    "revision": "54546d125034cb547ace",
+    "url": "/static/js/runtime-main.00e4f4ba.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  }
+]);
